9164566c5b2d3f245301e183ab4748c88367240e
dingdj <18950295115@163.com>
2016-06-30 16:52:32 +0800

    initial commit

---
+++ app/src/main/java/com/makedream/jawasi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.makedream.jawasi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
